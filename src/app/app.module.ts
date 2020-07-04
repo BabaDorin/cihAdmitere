b183dfac9c46e65b7871032c58057935ccb3e370
Baba Dorin <bvd.dorin@gmail.com>
2020-07-04 15:11:26 +0300

    routing and stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DateComponent } from './date/date.component';
+import { TimeComponent } from './time/time.component';
+import { AppointmentFormComponent } from './appointment-form/appointment-form.component';
+import { SpecialityComponent } from './speciality/speciality.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DateComponent,
+    TimeComponent,
+    AppointmentFormComponent,
+    SpecialityComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'specialitate', component: SpecialityComponent},
+      { path: 'data', component: DateComponent},
+      { path: 'formular', component: AppointmentFormComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
