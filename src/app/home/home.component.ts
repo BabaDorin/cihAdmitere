6040bb2cc3b8ed48e4016bbda0e900de57700032
Baba Dorin <bvd.dorin@gmail.com>
2020-07-05 18:55:57 +0300

    small change

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,9 +11,9 @@ export class HomeComponent implements OnInit {
   constructor(private databaseOp: DatabaseOperationsService) { 
     
     
-    console.log('gonna init');
-    databaseOp.init();
-    console.log('done');
+    // console.log('gonna init');
+    // databaseOp.init();
+    // console.log('done');
   }
 
   ngOnInit(): void {
